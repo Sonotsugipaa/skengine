ec4872e6e41ca73d534663d0150dc5bfd2be0054
Parola Marco <mark.prl.97@gmail.com>
2023-09-17 09:29:01 +0200

    i forgor 💀

---
+++ src/cxx/engine/atomic_id_gen.inl.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cassert>
+#include <idgen.hpp>
+
+
+
+namespace SKENGINE_NAME_NS {
+
+	template <idgen::Id T>
+	struct IdGeneratorWrapper {
+		std::mutex            mutex;
+		idgen::IdGenerator<T> gen;
+
+		T    generate() noexcept { mutex.lock(); auto r = gen.generate(); mutex.unlock(); return r; }
+		void recycle(T id)  noexcept { mutex.lock(); gen.recycle(id); mutex.unlock(); }
+	};
+
+	template <idgen::Id T>
+	IdGeneratorWrapper<T> id_generator;
+
+}
