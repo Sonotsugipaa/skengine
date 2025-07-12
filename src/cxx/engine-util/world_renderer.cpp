Sonotsugipaa/skengine

commit c53b2f8df78efdc28b0c15478eb34e9454ce3cfa

Fixed a bad Vulkan call when a draw command buffer is empty

src/cxx/engine-util/world_renderer.cpp
@@ -577,6 +577,7 @@ namespace SKENGINE_NAME_NS {
 				auto  batches  = objStorage.getDrawBatches();
 				auto& gfOsData = wgf.osData[osIdx];
 
+				if(gfOsData.objIdBfCopy.second < 1) continue;
 				if(batches.empty()) continue;
 
 				ModelId    last_mdl = ModelId    (~ model_id_e    (batches.front().model_id));

src/cxx/sneka3d/main.cpp
@@ -885,8 +885,10 @@ namespace sneka {
 					modObj.position_xyz.y = height;
 					modObj.direction_ypr.x += deltaAvg * tickreg::delta_t(2.0);
 					modObj.direction_ypr.x = modObj.direction_ypr.x - (std::floor(modObj.direction_ypr.x / PI2) * PI2);
-					auto& modLgt = rproc->worldRenderer()->modifyPointLight(pt.second.second);
-					modLgt.position.y = height + 0.5;
+					if(pt.second.second != idgen::invalidId<ske::ObjectId>()) {
+						auto& modLgt = rproc->worldRenderer()->modifyPointLight(pt.second.second);
+						modLgt.position.y = height + 0.5;
+					}
 				}
 			}
 

commit 8027a0964012601762b3ebd8507b74cc620c304f

Fixed incorrect rendering caused by commit c53b2f8

src/cxx/engine-util/world_renderer.cpp
@@ -494,9 +494,10 @@ namespace SKENGINE_NAME_NS {
 			// Create the buffers for draw command copies
 			for(size_t i = 0; auto& os : *mState.objectStorages) {
 				wgf.osData.push_back({ });
+				auto  drawCount = os.getDrawCount();
 				auto& data = wgf.osData.back();
-				world::resize_obj_buffer     (vma, &data.objBfCopy,     os.getDrawCount());
-				world::resize_obj_id_buffer  (vma, &data.objIdBfCopy,   os.getDrawCount());
+				world::resize_obj_buffer     (vma, &data.objBfCopy,     drawCount);
+				world::resize_obj_id_buffer  (vma, &data.objIdBfCopy,   drawCount);
 				world::resize_draw_cmd_buffer(vma, &data.drawCmdBfCopy, os.getDrawBatchCount());
 				data.cullPassUbo = world::create_cull_pass_ubo(vma);
 				++ i;
@@ -572,13 +573,15 @@ namespace SKENGINE_NAME_NS {
 
 		VkDescriptorSet dsets[]  = { wgf.frameDset, { } };
 		auto draw = [&](uint32_t subpassIdx) {
-			for(size_t osIdx = 0; auto& objStorage: objStorages) {
+			for(size_t osIdx = 0; osIdx < objStorages.size(); ++ osIdx) {
 				assert(osIdx < wgf.osData.size());
-				auto  batches  = objStorage.getDrawBatches();
-				auto& gfOsData = wgf.osData[osIdx];
+				auto& objStorage = objStorages[osIdx];
+				auto  batches    = objStorage.getDrawBatches();
+				auto& gfOsData   = wgf.osData[osIdx];
 
-				if(gfOsData.objIdBfCopy.second < 1) continue;
-				if(batches.empty()) continue;
+				if(objStorage.getDrawCount() < 1) continue;
+				assert(! batches.empty());
+				assert(gfOsData.objIdBfCopy.second > 0);
 
 				ModelId    last_mdl = ModelId    (~ model_id_e    (batches.front().model_id));
 				MaterialId last_mat = MaterialId (~ material_id_e (batches.front().material_id));
@@ -608,7 +611,6 @@ namespace SKENGINE_NAME_NS {
 						sizeof(VkDrawIndexedIndirectCommand) );
 					++ batchIdx;
 				}
-				++ osIdx;
 			}
 		};
 

src/cxx/engine-util/world_renderer_prepare.cpp
@@ -169,11 +169,12 @@ namespace SKENGINE_NAME_NS {
 			assert(wgf.osData.size() == objStorages.size());
 			for(size_t osIdx = 0; auto& os : objStorages) {
 				auto& gfOsData = wgf.osData[osIdx];
-				size_t objBytes   = os.getDrawCount()      * sizeof(dev::Object);
-				size_t objIdBytes = os.getDrawCount()      * sizeof(dev::ObjectId);
+				auto   drawCount  = os.getDrawCount();
+				size_t objBytes   = drawCount              * sizeof(dev::Object);
+				size_t objIdBytes = drawCount              * sizeof(dev::ObjectId);
 				size_t cmdBytes   = os.getDrawBatchCount() * sizeof(VkDrawIndexedIndirectCommand);
-				world::resize_obj_buffer     (vma, &gfOsData.objBfCopy,     os.getDrawCount());
-				world::resize_obj_id_buffer  (vma, &gfOsData.objIdBfCopy,   os.getDrawCount());
+				world::resize_obj_buffer     (vma, &gfOsData.objBfCopy,     drawCount);
+				world::resize_obj_id_buffer  (vma, &gfOsData.objIdBfCopy,   drawCount);
 				world::resize_draw_cmd_buffer(vma, &gfOsData.drawCmdBfCopy, os.getDrawBatchCount());
 				VkBufferMemoryBarrier2 bars[2] = { };
 				bars[0].sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER_2;